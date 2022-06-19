c9e8f0e2b1d272164585c9cea56cb7086ac09bda
jimmylid12 <jameslid12@msn.com>
2022-06-19 11:13:43 +0100

    Initial Commit

---
+++ Housing Application/Housing_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Housing_ApplicationApp.swift
+//  Housing Application
+//
+//  Created by James Liddle on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Housing_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
